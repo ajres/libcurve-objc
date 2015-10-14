5c334ea2d007c438c22ccb70df1f8c8420f6a772
Andrew Reslan <andrew@couchbase.com>
2015-10-14 11:13:29 +0100

    no message

---
+++ libcurve-objc/Dummy.h
@@ -0,0 +1,13 @@
+//
+//  YAActor+dummy.h
+//  libcurve-objc
+//
+//  Created by Andrew Reslan on 14/10/2015.
+//  Copyright © 2015 novoĉo. All rights reserved.
+//
+
+
+
+@interface Dummy : NSObject
+
+@end
---
+++ libcurve-objc/Dummy.m
@@ -0,0 +1,14 @@
+//
+//  YAActor+dummy.m
+//  libcurve-objc
+//
+//  Created by Andrew Reslan on 14/10/2015.
+//  Copyright © 2015 novoĉo. All rights reserved.
+//
+
+#include <Foundation/Foundation.h>
+#import "Dummy.h"
+
+@implementation Dummy
+
+@end
